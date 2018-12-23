29f37ea9ab722aea8af12457842ef5ac729f4035
logerk46 <logerk15a@gmail.com>
2018-12-23 19:40:39 +0200

    first commit

---
+++ src/components/PoetInfo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="poet-info__wrapper">
+            <div class="poet-info_title">
+                <h1>You are</h1>
+                <h2>{{poetData.title}}</h2>
+            </div>
+            <div class="poet-info_avatar">
+                <img :src="poetData.originalimage.source" width="230" height="285"/>
+            </div>
+            <div class="poet-info_body" v-html="poetData.extract_html">
+                {{poetData.extract_html}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['poetData'],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+    .poet-info__wrapper {
+        text-align: center;
+    }
+    .poet-info_body, .poet-info_avatar {
+        padding-top: 10px;
+    }
+</style>
